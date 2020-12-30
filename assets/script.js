31e0cd3718f25ba40072bdead9618d53204955c3
sorengrey <65551656+sorengrey@users.noreply.github.com>
2020-12-30 18:01:32 -0500

    Moved pwLength prompt
    Made pwLength the last prompt, per the homework instructions

--- assets/script.js
+++ assets/script.js
@@ -9,7 +9,6 @@ function writePassword() {
   passwordText.value = password;
 }
 
-
 // Clears the password if user clicks Generate Password multiple times
 function clearIt(){
    
@@ -17,9 +16,9 @@ function clearIt(){
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
-generateBtn.addEventListener("click", clearIt);
+//generateBtn.addEventListener("click", clearIt);
 
-// The character sets
+// character sets
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numerical = "0123456789"
@@ -27,41 +26,41 @@ var specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-='"
 var selected = ''
 var returnedPw = ''
 
-// The prompt that gets the number of characters - works, but needs to break after validation msg!
-var pwLength = prompt("Please enter a password length between 8 and 128 characters.")
-while (isNaN(pwLength) || pwLength <= 7 || pwLength >= 129){
- // The validation alert 
-    pwLength = alert("Please enter a number between 8 and 128.")
-}
-
-// The confirmation that asks about lowercase letters
+// offers lowercase letters
 var lowCase = confirm("Would you like to use lowercase letters?")
     if(lowCase){
    selected = selected.concat(lowercase);
  }
 
-// The prompt that offers uppercase letters
+// offers uppercase letters
 var upCase = confirm("Would you like to use uppercase letters?")
     if(upCase){
     selected = selected.concat(uppercase);
 }
 
-// The prompt that offers numbers
+// offers numbers
 var numCase = confirm("Would you like to use numbers?")
     if(numCase){
     selected = selected.concat(numerical);
 }
 
-// The prompt that offers special characters
+// offers special characters
 var symCase = confirm("Would you like to use special characters?")
     if(symCase){
-    selected =selected.concat(specialChars);
+    selected = selected.concat(specialChars);
 }
 
- //The alert that tells the user to click the Generate Password button
+// gets the number of characters - works, but needs to break after validation msg!
+var pwLength = prompt("Please enter a password length between 8 and 128 characters.")
+while (isNaN(pwLength) || pwLength <= 7 || pwLength >= 129){
+ //  validation alert 
+    pwLength = alert("Please enter a number between 8 and 128.")
+}
+
+// tells the user to click the Generate Password button
 var done = alert("All finished! Click Generate Password!")
 
-// The password generator function - works, but needs to stop
+// password generator function - works, but needs to stop
  function generatePassword() {
     for (i = 0, n = selected.length; i < pwLength; ++i) {
          returnedPw += selected.charAt(Math.floor(Math.random() * n));
