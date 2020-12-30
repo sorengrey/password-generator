73856cea24ed7eb891eb045bf95094154bf25bfd
sorengrey <65551656+sorengrey@users.noreply.github.com>
2020-12-30 17:12:29 -0500

    Moved alert, got generatePassword function working
    Moved the onload greeting to the HTML file
    Got the generatePassword function working for lowercase letters

--- assets/script.js
+++ assets/script.js
@@ -13,37 +13,56 @@ function writePassword() {
 generateBtn.addEventListener("click", writePassword);
 
 // The character sets
+//var lowercase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numerical = "0123456789"
 var specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-='"
+var charArr = ''
+var returnedPw = ''
 
-// The alert that greets the user
-var greetings = alert("Welcome! Please click OK to get started.");
-
-// The prompt that gets the number of characters
+// The prompt that gets the number of characters - works, but needs to break after validation msg!
 var pwLength = prompt("Please enter a password length between 8 and 128 characters.")
 while (isNaN(pwLength) || pwLength <= 7 || pwLength >= 129){
  // The validation alert 
     pwLength = alert("Please enter a number between 8 and 128.")
 }
 
-// The confirmation that asks about lowercase letters  
+// The confirmation that asks about lowercase letters - works!
 var lowCase = confirm("Would you like to use lowercase letters?")
+ if(lowCase){
+   charArr = charArr.concat(lowercase);
+ }
+console.log(charArr)
 
-// The prompt that asks if they want uppercase letters
+/* The prompt that asks if they want uppercase letters
 var upCase = confirm("Would you like to use uppercase letters?")
+if(lowCase){
+    charArr.push(uppercase);
+}
+
 
 // The prompt that asks if they want numbers
 var numCase = confirm("Would you like to use numbers?")
+if(numCase){
+    charArr.push(numerical);
+}
+
 
 // The prompt that asks if they want special characters
 var symCase = confirm("Would you like to use special characters?")
+if(symCase){
+    charArr.push(specialChars);
+}
 
-// The password generator function -- needs if statements -- saved from the old version, not working!
-/* function generatePassword() {
-    var returnedPw = " "
-    for (var i = 0, n = lowercase.length; i < pwLength; ++i) {
-        returnedPw += lowercase.charAt(Math.floor(Math.random() * n));
+*/
+ //The alert that tells the user to click the Generate Password button
+var done = alert("All finished! Click Generate Password!")
+
+// The password generator function - testing! Works for lowercase so far */
+ function generatePassword() {
+    for (i = 0, n = charArr.length; i < pwLength; ++i) {
+         returnedPw += charArr.charAt(Math.floor(Math.random() * n));
     }
-    return returnedPw; */
+    return returnedPw;
+ }
