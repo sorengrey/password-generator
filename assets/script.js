4cb92f7fc291557522d07d9f1409b811c7830d0b
sorengrey <65551656+sorengrey@users.noreply.github.com>
2020-12-31 13:21:17 -0500

    Changes to generatePassword()
    generatePassword() now only generates one password per click instead of appending the newly generated password

--- assets/script.js
+++ assets/script.js
@@ -7,16 +7,10 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
   passwordText.value = password;
-}
-
-// Clears the password if user clicks Generate Password multiple times
-function clearIt(){
-   
-}
+  }
 
 // Add event listener to generate button
-generateBtn.addEventListener("click", writePassword);
-//generateBtn.addEventListener("click", clearIt);
+generateBtn.addEventListener("click", writePassword)
 
 // character sets
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
@@ -50,6 +44,11 @@ var symCase = confirm("Would you like to use special characters?")
     selected = selected.concat(specialChars);
 }
 
+// tells the user they must select at least one character type - works, but needs to loop back to lowCase!
+while(lowCase === false && upCase === false && numCase === false && symCase === false){
+    var pickOne = alert("Please select at least one character type.")
+}
+
 // gets the number of characters - works, but needs to break after validation msg!
 var pwLength = prompt("Please enter a password length between 8 and 128 characters.")
 while (isNaN(pwLength) || pwLength <= 7 || pwLength >= 129){
@@ -60,10 +59,12 @@ while (isNaN(pwLength) || pwLength <= 7 || pwLength >= 129){
 // tells the user to click the Generate Password button
 var done = alert("All finished! Click Generate Password!")
 
-// password generator function - works, but needs to stop
+// password generator function - works, but needs to stop after one click!
  function generatePassword() {
+    var returnedPw = ''
     for (i = 0, n = selected.length; i < pwLength; ++i) {
          returnedPw += selected.charAt(Math.floor(Math.random() * n));
     }
     return returnedPw;
  }
+
