f7646a3abf507d8da54cd7f96de1064ccc9c0c8b
sorengrey <65551656+sorengrey@users.noreply.github.com>
2021-01-02 16:24:11 -0500

    Final commit
    Wrapped four character set questions in a while loop so that they could repeat if the user does not select a character type.
    Changed order of prompts so that the character length question is asked first.
    Minor changes to comments.

--- assets/script.js
+++ assets/script.js
@@ -15,38 +15,9 @@ generateBtn.addEventListener("click", writePassword)
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numerical = "0123456789"
-var specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-='"
+var specialChars = "!@#$%^&*()_+~`|}{[]:;?><,./-='"
 var selected = ''
 var returnedPw = ''
-
-// Offers lowercase letters
-var lowCase = confirm("Would you like to use lowercase letters?")
-    if(lowCase){
-   selected = selected.concat(lowercase);
- }
-
-// Offers uppercase letters
-var upCase = confirm("Would you like to use uppercase letters?")
-    if(upCase){
-    selected = selected.concat(uppercase);
-}
-
-// Offers numbers
-var numCase = confirm("Would you like to use numbers?")
-    if(numCase){
-    selected = selected.concat(numerical);
-}
-
-// Offers special characters
-var symCase = confirm("Would you like to use special characters?")
-    if(symCase){
-    selected = selected.concat(specialChars);
-}
-
-// Tells the user they must select at least one character type
-while(lowCase === false && upCase === false && numCase === false && symCase === false){
-    var pickOne = alert("Please select at least one character type.")
-}
 
 // Gets the number of characters
 var pwLength = prompt("Please enter a password length between 8 and 128 characters.")
@@ -57,15 +28,50 @@ while (isNaN(pwLength) || pwLength <= 7 || pwLength >= 129){
     pwLength = prompt("Please enter a password length between 8 and 128 characters.")
 }
 
+// Sets the user's inputs to false so the while loop can start the prompts over if the user doesn't choose a character set
+var lowCase = false;
+var upCase = false;
+var numCase = false;
+var symCase = false;
+    
+// Loops through four questions until the user confirms at least one
+while(lowCase === false && upCase === false && numCase === false && symCase === false){
+    // Offers lowercase letters
+    var lowCase = confirm("Would you like to use lowercase letters?")
+      if(lowCase){
+          selected = selected.concat(lowercase);
+}
+// Offers uppercase letters
+    var upCase = confirm("Would you like to use uppercase letters?")
+      if(upCase){
+        selected = selected.concat(uppercase);
+}
+// Offers numbers
+    var numCase = confirm("Would you like to use numbers?")
+      if(numCase){
+        selected = selected.concat(numerical);
+}   
+// Offers special characters
+    var symCase = confirm("Would you like to use special characters?")
+      if(symCase){
+        selected = selected.concat(specialChars);
+}
+// Tells the user they must select at least one character type
+if (lowCase === false && upCase === false && numCase === false && symCase === false){
+    // Validation alert
+    var pickOne = alert("Please select at least one character type.")
+ }
+}
+
 // Tells the user to click the Generate Password button
 var done = alert("All finished! Click Generate Password!")
 
 // Password generator function
- function generatePassword() {
+function generatePassword() {
     var returnedPw = ''
     for (i = 0, n = selected.length; i < pwLength; ++i) {
          returnedPw += selected.charAt(Math.floor(Math.random() * n));
     }
     return returnedPw;
- }
+}
 
