d4374d41621f967537d45271d577df969139c18b
sorengrey <65551656+sorengrey@users.noreply.github.com>
2021-01-02 13:42:50 -0500

    Comments cleaned up
    Removed notes to self and made capitalization consistent

--- assets/script.js
+++ assets/script.js
@@ -1,4 +1,3 @@
-
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
@@ -12,7 +11,7 @@ function writePassword() {
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword)
 
-// character sets
+// Character sets
 var lowercase = "abcdefghijklmnopqrstuvwxyz"
 var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 var numerical = "0123456789"
@@ -20,47 +19,48 @@ var specialChars = "!@#$%^&*()_+~`|}{[]\:;?><,./-='"
 var selected = ''
 var returnedPw = ''
 
-// offers lowercase letters
+// Offers lowercase letters
 var lowCase = confirm("Would you like to use lowercase letters?")
     if(lowCase){
    selected = selected.concat(lowercase);
  }
 
-// offers uppercase letters
+// Offers uppercase letters
 var upCase = confirm("Would you like to use uppercase letters?")
     if(upCase){
     selected = selected.concat(uppercase);
 }
 
-// offers numbers
+// Offers numbers
 var numCase = confirm("Would you like to use numbers?")
     if(numCase){
     selected = selected.concat(numerical);
 }
 
-// offers special characters
+// Offers special characters
 var symCase = confirm("Would you like to use special characters?")
     if(symCase){
     selected = selected.concat(specialChars);
 }
 
-// tells the user they must select at least one character type - works, but needs to loop back to lowCase!
+// Tells the user they must select at least one character type
 while(lowCase === false && upCase === false && numCase === false && symCase === false){
     var pickOne = alert("Please select at least one character type.")
 }
 
-// gets the number of characters - works, but needs to break after validation msg!
+// Gets the number of characters
 var pwLength = prompt("Please enter a password length between 8 and 128 characters.")
 while (isNaN(pwLength) || pwLength <= 7 || pwLength >= 129){
- //  validation alert 
+ //  Validation alert 
     alert("Please enter a number between 8 and 128.")
+ // Lets the user try again   
     pwLength = prompt("Please enter a password length between 8 and 128 characters.")
 }
 
-// tells the user to click the Generate Password button
+// Tells the user to click the Generate Password button
 var done = alert("All finished! Click Generate Password!")
 
-// password generator function
+// Password generator function
  function generatePassword() {
     var returnedPw = ''
     for (i = 0, n = selected.length; i < pwLength; ++i) {
